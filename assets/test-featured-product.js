12cb44ea43acb1a7fbac5053cf6063296b37d86b
Just-Maverik <stanislav.sidorov@binargear.com>
2024-04-17 15:31:08 +0300

    Featured product test

---
+++ assets/test-featured-product.js
@@ -0,0 +1,63 @@
+let variantButtons = document.querySelectorAll('.variant-btn');
+if(variantButtons) {
+  variantButtons.forEach((item) =>{
+    item.addEventListener('click', function() {
+      let variantId = this.getAttribute('data-variant-id');
+      let parent = this.parentNode;
+      let MainParent = parent.parentNode;
+      parent.querySelectorAll('.variant-btn').forEach(btn => btn.classList.remove('active'));
+      this.classList.add('active');
+      MainParent.querySelector('.add-to-cart-btn').setAttribute('data-variant-id', variantId);
+    });
+  })
+
+}
+
+let addButtons = document.querySelectorAll('.add-to-cart-btn');
+if ( addButtons) {
+  addButtons.forEach((item) => {
+    item.addEventListener('click', function() {
+      let custom_tag = item.getAttribute('data-custom-tag');
+      let variantId = this.getAttribute('data-variant-id');
+      let originalText = this.innerText;
+      this.innerText = 'Adding';
+      this.disabled = true;
+      let object = {};
+
+      if(custom_tag === 'true') {
+        object = {
+          id: variantId,
+          quantity: 1,
+          properties: {
+            custom_tag: 'bought_featured'
+          }
+        }
+      } else {
+        object = {
+          id: variantId,
+          quantity: 1
+        }
+      }
+
+      fetch('/cart/add.js', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(object)
+      })
+          .then(response => {
+            this.innerText = 'Added';
+            setTimeout(() => {
+              this.innerText = originalText;
+              this.disabled = false;
+            }, 1500);
+          })
+          .catch(error => {
+            console.error('Error adding product to cart:', error);
+            this.innerText = originalText;
+            this.disabled = false;
+          });
+    });
+  })
+}
